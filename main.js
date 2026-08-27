168c6ef56193eb68547552cb455fc07480a3202c
kimyongho1112 <guid4701@naver.com>
2026-08-27 19:28:07 +0900

    Update portfolio pages

---
+++ main.js
@@ -0,0 +1,32 @@
+const menuButton = document.querySelector(".menu-button");
+const menuOverlay = document.getElementById("menuOverlay");
+const menuClose = document.getElementById("menuClose");
+const menuLinks = document.querySelectorAll(".menu-link");
+
+function openMenu() {
+  menuOverlay.classList.add("is-open");
+  document.body.classList.add("menu-open");
+}
+
+function closeMenu() {
+  menuOverlay.classList.remove("is-open");
+  document.body.classList.remove("menu-open");
+}
+
+if (menuButton) {
+  menuButton.addEventListener("click", openMenu);
+}
+
+if (menuClose) {
+  menuClose.addEventListener("click", closeMenu);
+}
+
+menuLinks.forEach((link) => {
+  link.addEventListener("click", closeMenu);
+});
+
+document.addEventListener("keydown", (event) => {
+  if (event.key === "Escape") {
+    closeMenu();
+  }
+});
